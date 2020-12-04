8e07b46ed56cfcffc27f3daf44b56c701ef729d0
damian0s10 <damian.wasiluk10@gmail.com>
2020-12-04 10:43:22 +0100

    change place script

--- index.js
+++ index.js
@@ -7,28 +7,3 @@ window.onload = () => {
                 .then(function() { console.log('Service Worker Registered'); });
     }
 };
-function notifyMe() {
-    console.log("Powiadomienie");
-    // Sprawdzamy czy przeglądarka obsługuje powiadomienia.
-    if (!("Notification" in window)) {
-      alert("Ta przeglądarka nie obsługuje powiadomień");
-    }
-  
-    // Sprawdźmy czy uprawnienia dla powiadomienia zostały nadane
-    else if (Notification.permission === "granted") {
-      // jeżeli są tworzymy powiadomienie
-      var notification = new Notification("hello world!");
-    }
-  
-    // W innym przypadku tworzymy zapytanie o uprawnienia
-    else if (Notification.permission !== 'denied') {
-      Notification.requestPermission(function (permission) {
-        //Jeżeli użytkownik zaakceptuje tworzymy powiadomienie
-        if (permission === "granted") {
-          var notification = new Notification("hello world!");
-        }
-      });
-    }
-  
-   
-  }
