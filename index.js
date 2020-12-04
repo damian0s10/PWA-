708c61a642729a9a1e801b80450349c08eb484cd
damian0s10 <damian.wasiluk10@gmail.com>
2020-12-04 10:36:59 +0100

    notifications

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ window.onload = () => {
                 .then(function() { console.log('Service Worker Registered'); });
     }
 };
+function notifyMe() {
+    // Sprawdzamy czy przeglądarka obsługuje powiadomienia.
+    if (!("Notification" in window)) {
+      alert("Ta przeglądarka nie obsługuje powiadomień");
+    }
+  
+    // Sprawdźmy czy uprawnienia dla powiadomienia zostały nadane
+    else if (Notification.permission === "granted") {
+      // jeżeli są tworzymy powiadomienie
+      var notification = new Notification("hello world!");
+    }
+  
+    // W innym przypadku tworzymy zapytanie o uprawnienia
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        //Jeżeli użytkownik zaakceptuje tworzymy powiadomienie
+        if (permission === "granted") {
+          var notification = new Notification("hello world!");
+        }
+      });
+    }
+  
+   
+  }
